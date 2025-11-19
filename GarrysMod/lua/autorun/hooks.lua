b808320cacd7e6e3099f91129ba5f0b6b3dfd9ed
Hgbddn <bralgo.zemalgo@gmail.com>
2025-11-19 07:53:23 +0100

    Add files via upload

---
+++ GarrysMod/lua/autorun/hooks.lua
@@ -0,0 +1,45 @@
+hook.Add( "PlayerButtonDown", "Model", function( ply, button )
+    if button != KEY_P then return end
+
+    if CLIENT and not IsFirstTimePredicted() then
+        return
+    end
+
+    RunConsoleCommand("playermodel_selector")
+end)
+
+
+
+hook.Add( "PlayerButtonDown", "Spectator End", function( ply, button )
+    if button != KEY_L then return end
+
+    if CLIENT and not IsFirstTimePredicted() then
+        return
+    end
+
+    RunConsoleCommand("ttt_spectate", "0")
+end)
+
+
+
+hook.Add( "PlayerButtonDown", "MOTD", function( ply, button )
+    if button != KEY_M then return end
+
+    if CLIENT and not IsFirstTimePredicted() then
+        return
+    end
+
+    RunConsoleCommand("ulx", "motd")
+end)
+
+
+
+hook.Add( "PlayerButtonDown", "Kill", function( ply, button )
+    if button != KEY_K then return end
+
+    if CLIENT and not IsFirstTimePredicted() then
+        return
+    end
+
+    RunConsoleCommand("kill")
+end)
